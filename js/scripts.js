ac8171583346aa620482f5ff2ce6c15d68a34c2f
Cory Meyers and Michael Dada <mail.michaeldada@gmail.com>
2016-01-25 11:52:25 -0800

    add functionality for addresses

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,77 @@ function Contact(firstName, lastName) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+function Location(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+  this.addresses = [];
+}
+
+
+Location.prototype.fullLocation = function() {
+  return this.street + ", " + this.city + ", " + this.state;
+}
+
+$(document).ready(function() {
+  $("#add-address").click (function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
+  $("form#new-contact").submit(function(event) {
+  event.preventDefault();
+
+  var inputtedFirstName = $("input#new-first-name").val();
+  var inputtedLastName = $("input#new-last-name").val();
+  var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+  $(".new-address").each(function() {
+  var inputtedStreet = $(this).find("input.new-street").val();
+  var inputtedCity = $(this).find("input.new-city").val();
+  var inputtedState = $(this).find("input.new-state").val();
+  var newLocation = new Location(inputtedStreet, inputtedCity, inputtedState)
+  newContact.addresses.push(newLocation)
+});
+
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+
+  $(".contact").last().click(function(){
+  $("#show-contact").show();
+  $("#show-contact h2").text(newContact.fullName());
+  $(".first-name").text(newContact.firstName);
+  $(".last-name").text(newContact.lastName);
+
+  $("ul#addresses").text("");
+newContact.addresses.forEach(function(address) {
+  $("ul#addresses").append("<li>" + address.fullLocation() + "</li>");
+
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+  $("input.addresses").val("");
+  $("input.new-street").val("");
+   $("input.new-city").val("");
+   $("input.new-state").val("");
+
+
+});
+
+
+  });
+  });
+
+});
--- spec/specs.js
+++ spec/specs.js
@@ -5,8 +5,19 @@ describe('Contact', function() {
     expect(testContact.lastName).to.equal("Moreno");
     expect(testContact.addresses).to.eql([]);
   });
+
   it("adds the full name to a contact", function(){
     var testContact = new Contact("Sherlock", "Holmes");
     expect(testContact.fullName()).to.equal("Sherlock Holmes");
+  });
+});
+
+describe('Location', function() {
+  it("creates a new location with the given properties", function() {
+    var testLocation = new Location("123 Anyplace Street", "Anywhere", "OR");
+    expect(testLocation.street).to.equal("123 Anyplace Street");
+    expect(testLocation.city).to.equal("Anywhere");
+    expect(testLocation.state).to.equal("OR");
+    expect(testLocation.addresses).to.eql([]);
   });
 });
